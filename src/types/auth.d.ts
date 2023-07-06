3d718e43a0be51565932a8220449e3a4fecab62b
ArcadeArchie <29282748+ArcadeArchie@users.noreply.github.com>
2023-07-07 00:47:26 +0200

    fixed typing errors had to use an workaround, issue is already tracked in upstream as #6640

--- src/types/auth.d.ts
+++ src/types/auth.d.ts
@@ -1,10 +1,17 @@
-import { DefaultSession } from "@auth/core/types";
+import type { DefaultSession } from "@auth/core/types";
+import type { JWT } from "@auth/core/jwt";
 
-declare module "@auth/core/types" {
+declare module "../../node_modules/.pnpm/@auth+core@0.9.0/node_modules/@auth/core/types" {
   interface Session {
-    user: {
-      id?: string | null;
+    user?: {
+      id: string;
     } & DefaultSession['user'];
-    accessToken: string;
+    accessToken: string | undefined;
+  }
+}
+declare module "../../node_modules/.pnpm/@auth+core@0.9.0/node_modules/@auth/core/jwt" {
+  interface JWT {
+    id: string;
+    accessToken: string | undefined;
   }
 }
